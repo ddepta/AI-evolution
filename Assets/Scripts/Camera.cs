28d8a5a0a7509ed475fe6403bdcac9b46f8f8c3d
Daniel Depta <56829289+ddepta@users.noreply.github.com>
2023-02-10 16:17:31 +0100

    third person moving camera for player

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform Player;
+    private Vector3 initialPlayerPos;
+    private Vector3 initialCameraPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPlayerPos = Player.position;
+        initialCameraPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        var playerOffset = Player.position - initialPlayerPos;
+        transform.position = initialCameraPos + playerOffset;
+    }
+}
